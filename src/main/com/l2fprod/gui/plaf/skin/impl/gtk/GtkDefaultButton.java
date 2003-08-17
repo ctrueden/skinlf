52669a5e25c5606c3222d1f1a2e7f16514d0fde4
l2fprod <l2fprod@184bcedb-ef5e-6bdb-aea7-fde92d8c4e55>
2003-08-17 18:02:00 +0000

    initial checkin
    git-svn-id: https://svn.java.net/svn/skinlf~svn/trunk@28 184bcedb-ef5e-6bdb-aea7-fde92d8c4e55

---
+++ src/main/com/l2fprod/gui/plaf/skin/impl/gtk/GtkDefaultButton.java
@@ -0,0 +1,95 @@
+/* ====================================================================
+ *
+ * @PROJECT.FULLNAME@ @VERSION@ License.
+ *
+ * Copyright (c) @YEAR@ L2FProd.com.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by L2FProd.com
+ *        (http://www.L2FProd.com/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "@PROJECT.FULLNAME@", "SkinLF" and "L2FProd.com" must not
+ *    be used to endorse or promote products derived from this software
+ *    without prior written permission. For written permission, please
+ *    contact info@L2FProd.com.
+ *
+ * 5. Products derived from this software may not be called "SkinLF"
+ *    nor may "SkinLF" appear in their names without prior written
+ *    permission of L2FProd.com.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL L2FPROD.COM OR ITS CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ */
+package com.l2fprod.gui.plaf.skin.impl.gtk;
+
+import com.l2fprod.gui.plaf.skin.DefaultButton;
+import com.l2fprod.gui.icon.ArrowIcon;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.SwingConstants;
+
+class GtkDefaultButton extends DefaultButton {
+
+  /** mainly used by optionmenu */
+  GtkProps parserItem;
+
+  public GtkDefaultButton(Image bitmap, int imageWidth, int imageHeight,
+                          int topHeight, int rightWidth, int bottomHeight, int leftWidth) {
+    super(bitmap, imageWidth, imageHeight,
+          topHeight, rightWidth, bottomHeight, leftWidth, false);
+  }
+
+  public GtkDefaultButton(Image top, Image bottom, Image left, Image right,
+                          Image topLeft, Image topRight, Image bottomLeft, Image bottomRight) {
+    super(top, bottom, left, right,
+          topLeft, topRight, bottomLeft, bottomRight);
+  }
+
+  public GtkDefaultButton(Image bitmap, int imageWidth, int imageHeight,
+                          int topHeight, int rightWidth, int bottomHeight, int leftWidth, boolean tile) {
+    super(bitmap, imageWidth, imageHeight,
+          topHeight, rightWidth, bottomHeight, leftWidth, tile);
+  }
+      
+  public void paint(Graphics g, Component c) {
+    super.paint(g, c);
+    if ("false".equals(parserItem.getProperty("arrow"))) {
+      ArrowIcon.paintArrow(g, 5,
+                           c.getWidth() - rightWidth,
+                           c.getHeight() / 2 - 3,
+                           rightWidth,
+                           c.getHeight(),
+                           SwingConstants.SOUTH,
+                           c.isEnabled());
+    }
+  }
+
+}
+
