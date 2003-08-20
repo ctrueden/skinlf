449c8f9a4ebb4d068efb1aebb465f7b2c52e6701
l2fprod <l2fprod@184bcedb-ef5e-6bdb-aea7-fde92d8c4e55>
2003-08-20 09:35:36 +0000

    move skinit code in a package
    git-svn-id: https://svn.java.net/svn/skinlf~svn/trunk@51 184bcedb-ef5e-6bdb-aea7-fde92d8c4e55

--- src/main/Skinit.java
+++ src/main/Skinit.java
@@ -45,28 +45,17 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ====================================================================
  */
-import java.lang.reflect.Method;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeEvent;
-import javax.swing.UIManager;
-import javax.swing.LookAndFeel;
-import javax.swing.plaf.metal.*;
-
-import com.l2fprod.gui.plaf.skin.SkinLookAndFeel;
-import com.l2fprod.gui.plaf.skin.Skin;
-import com.l2fprod.gui.plaf.skin.SkinUtils;
-import com.l2fprod.gui.plaf.skin.CompoundSkin;
 
 /**
- * Skinit. <br>
- * Skinit is Skin Look And Feel wrapper. It allows you to start any application
- * with SkinLF. Skinit will prevent the application to set its own Look And
- * Feel. Once Skin Look And Feel is set, it can't be removed. <br>
+ * Skinit. <br> Skinit is Skin Look And Feel wrapper. It allows you to
+ * start any application with SkinLF. Skinit will prevent the
+ * application to set its own Look And Feel. Once Skin Look And Feel
+ * is set, it can't be removed. <br>
  *
  * @author    $Author: l2fprod $
- * @version   $Revision: 1.1 $, $Date: 2003-08-01 20:40:15 $
+ * @version   $Revision: 1.2 $, $Date: 2003-08-20 09:35:36 $
  */
-public class Skinit extends javax.swing.JApplet {
+public class Skinit extends com.l2fprod.gui.plaf.skin.Skinit {
 
   /**
    * The main program for the Skinit class
@@ -75,167 +64,7 @@ public class Skinit extends javax.swing.JApplet {
    * @exception Exception  Description of Exception
    */
   public static void main(String[] args) throws Exception {
-
-    if (args.length == 0) {
-      printUsage();
-    }
-
-    int mainClassNameIndex = -1;
-    String gtktheme = null;
-    String kdetheme = null;
-    String packtheme = null;
-    String lnfclassname = null;
-    String metaltheme = null;
-
-    for (int i = 0, c = args.length; i < c; i++) {
-      if (args[i].equals("-gtk")) {
-        gtktheme = args[++i];
-      }
-      else if (args[i].equals("-kde")) {
-        kdetheme = args[++i];
-      }
-      else if (args[i].equals("-pack")) {
-        packtheme = args[++i];
-      }
-      else if (args[i].equals("-lnf")) {
-        lnfclassname = args[++i];
-      }      
-      else if (args[i].equals("-metaltheme")) {
-        metaltheme = args[++i];
-      }      
-      else {
-        mainClassNameIndex = i;
-        break;
-      }
-    }
-
-    String[] realArgs = new String[args.length - mainClassNameIndex - 1];
-    for (int i = 0, c = realArgs.length; i < c; i++) {
-      realArgs[i] = args[mainClassNameIndex + i + 1];
-    }
-
-    // First try to find the class
-    Class clazz = null;
-    try {
-      clazz = Class.forName(args[mainClassNameIndex]);       
-    } catch (ClassNotFoundException e) {
-      System.err.println("The class " + args[mainClassNameIndex] + " was not found in the classpath.");
-      System.exit(1);
-    } catch (Throwable e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-    
-    // if the class exists, get the main method
-    Method mainMethod = null;
-    try {
-      mainMethod = clazz.getMethod("main", new Class[]{String[].class});
-    } catch (NoSuchMethodException e) {
-      System.err.println("No method public static void main(String[] args) in " + clazz.getName());
-      System.exit(1);
-    } catch (Throwable e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-    
-    // try to make sure the main method is accessible
-    try {
-      mainMethod.setAccessible(true);
-    } catch (Throwable e) {      
-    }
-
-    // main class and main method found, time to load the skin
-    if (lnfclassname != null) {
-      LookAndFeel lnf = (LookAndFeel)Class.forName(lnfclassname).newInstance();
-      UIManager.setLookAndFeel(lnf);
-    } else if (metaltheme != null) {
-      MetalTheme theme = (MetalTheme)Class.forName(metaltheme).newInstance();
-      MetalLookAndFeel metal = new MetalLookAndFeel();
-      metal.setCurrentTheme(theme);
-      UIManager.setLookAndFeel(metal);
-    } else {
-      Skin skin = null;
-      
-      if (packtheme != null) {
-        if (SkinUtils.DEBUG) {
-          System.out.println("Loading themepack " + packtheme);
-        }
-        skin = SkinLookAndFeel.loadThemePack(packtheme);
-      }
-      else if (gtktheme != null) {
-        if (kdetheme != null) {
-          skin = new CompoundSkin(SkinLookAndFeel.loadSkin(gtktheme),
-                                  SkinLookAndFeel.loadSkin(kdetheme));
-        }
-        else {
-          skin = SkinLookAndFeel.loadSkin(gtktheme);
-        }
-      }
-      
-      /*
-       *  try to use the user default skin
-       */
-      if (skin == null) {
-        if (SkinUtils.DEBUG) {
-          System.out.println("Trying user skin");
-        }
-        skin = SkinLookAndFeel.getSkin();
-      }
-      
-      if (skin != null) {
-        SkinLookAndFeel.setSkin(skin);
-        SkinLookAndFeel lnf = new SkinLookAndFeel();
-        UIManager.setLookAndFeel(lnf);
-        
-        UIManager.
-          addPropertyChangeListener(new PropertyChangeListener() {
-              public void propertyChange(PropertyChangeEvent event) {
-                Object newLF = event.getNewValue();
-                
-                if ((newLF instanceof SkinLookAndFeel) == false) {
-                  try {
-                    UIManager.setLookAndFeel(new SkinLookAndFeel());
-                  } catch (Exception e) {
-                    e.printStackTrace();
-                  }
-                }
-                
-              }
-            });
-        
-      }
-      else {
-        System.out.println("No GTK theme provided, defaulting to application Look And Feel");
-      }
-    }
-
-    try {
-      mainMethod.invoke(null, new Object[]{realArgs});       
-    } catch (IllegalAccessException e) {
-      System.err.println("Please make sure the class " + clazz.getName() +
-                         " and the method main(String[] args) are public.");
-      System.exit(1);
-    } catch (Throwable e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-  }
-
-  /**
-   * Description of the Method
-   */
-  static void printUsage() {
-    String usage = "Skinit - Skin Look And Feel wrapper\n" +
-        "Usage: skinit [options] class [args...]\n" +
-        "\n" +
-        "where options include:\n" +
-        "\t-gtk <name>              GTK Theme Filename\n" +
-        "\t-kde <name>              KDE Theme Filename\n" +
-        "\t-pack <name>             Theme Pack Filename\n" +
-        "\t-lnf <classname>         Look and feel class name\n" +
-        "\t-metaltheme <classname>  Metal Theme to use\n";
-    System.out.println(usage);
-    System.exit(1);
+    com.l2fprod.gui.plaf.skin.Skinit.main(args);
   }
 
 }
