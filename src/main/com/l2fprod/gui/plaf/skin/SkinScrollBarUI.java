ba2b36b06a54c3cf85dd2a1768da1e7950d850d8
l2fprod <l2fprod@184bcedb-ef5e-6bdb-aea7-fde92d8c4e55>
2009-05-01 13:26:04 +0000

    ASL 2.0
    git-svn-id: https://svn.java.net/svn/skinlf~svn/trunk@271 184bcedb-ef5e-6bdb-aea7-fde92d8c4e55

--- src/main/com/l2fprod/gui/plaf/skin/SkinScrollBarUI.java
+++ src/main/com/l2fprod/gui/plaf/skin/SkinScrollBarUI.java
@@ -1,49 +1,19 @@
-/* ====================================================================
- *
+/**
  * @PROJECT.FULLNAME@ @VERSION@ License.
  *
- * Copyright (c) @YEAR@ L2FProd.com.  All rights reserved.
+ * Copyright @YEAR@ L2FProd.com
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowlegement:
- *       "This product includes software developed by L2FProd.com
- *        (http://www.L2FProd.com/)."
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
- *
- * 4. The names "@PROJECT.FULLNAME@", "SkinLF" and "L2FProd.com" must not
- *    be used to endorse or promote products derived from this software
- *    without prior written permission. For written permission, please
- *    contact info@L2FProd.com.
- *
- * 5. Products derived from this software may not be called "SkinLF"
- *    nor may "SkinLF" appear in their names without prior written
- *    permission of L2FProd.com.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL L2FPROD.COM OR ITS CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.l2fprod.gui.plaf.skin;
 
@@ -61,7 +31,7 @@ import javax.swing.plaf.ComponentUI;
 /**
  * @author    $Author: l2fprod $
  * @created   27 avril 2002
- * @version   $Revision: 1.2 $, $Date: 2003-12-06 21:53:26 $
+ * @version   $Revision: 1.3 $, $Date: 2009-05-01 13:24:44 $
  */
 public final class SkinScrollBarUI extends XTraScrollBarUI {
 
